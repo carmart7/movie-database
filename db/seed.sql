e92e89c20d9b5d4cea88b403bbbde98eee09e3ee
[carlos martinez] <theycallhellp@gmail.com>
2022-10-31 12:35:20 -0700

    mysql schema and seeding

---
+++ db/seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO movies (movie_name)
+VALUES  ('Iron Man'),
+        ('Thor'),
+        ('Doctor Strange'),
+        ('Guardians of the Galaxy'),
+        ('Spider-man'),
+        ('Venom'),
+        ('Captain America: Civil War');
+
+INSERT INTO reviews (movie_id, review)
+VALUES  (1, "This is the movie that started it all! and my first Mcu movie.So I watched it when I was a kid on my Dad's laptop and I was blown away by the CGI and the engaging story.For a long time after watching the movie, I didn't want to look at any other incarnation of Iron Man because Robert Downey Jr made the role so unique it became a bit weird to look at other versions of Iron Man in the animated films or comics. I liked the villain, the iron monger I feel pushed him(iron man) to be a hero that the mcu deserved. For people who did not watch it, I think they should as seeing his origin story well serve you with deeper understanding of his character arc. All in all Iron Man is a good movie that would provide plenty of action, Romance and comedy for the audiences."),
+        (1, "Out of all the first 3 movies the mcu released this is easily the best one. Even though i dont like it as much anymore as i used to i still really like this movie and its one of the few movies of phase 1 that has a clear vision and knows what it wants to be (unlike the sequel). And also for 2008 the cgi and visual effects are surprisingly good and hold up very well. And while watching the movie i'm always really interested in Tony's suits in this movie and i think its cool that he wears 3 different suits in this one. So to sum it up its a nice and exciting start to the mcu and its far from a top 10 mcu movie but for phase 1 its really good."),
+        (2, "Who knew Thor was the perfect drinking buddy and is domesticated lol? Perfect writing. Perfect directing. Perfect casting. Perfect cinematography. Perfect amount of S/VFX. Perfect score. Perfect comedy/drama/thrills. Etc... 10/10"),
+        (3, "Lives up to its title, in mostly good ways."),
+        (3, "With Dr. Strange the Marvel magic seems largely to have run out-which is unfortunate, given the movie's titular protagonist is himself a magician."),
+        (4, "Was a sick flick"),
+        (4, "Blessed with a loose, anarchic B-picture soul that encourages you to enjoy yourself even when you're not quite sure what's going on, the scruffy Guardians is irreverent in a way that can bring the first Star Wars to mind."),
+        (5, "The first Spider-Man isnt the best of Raimis three Spidey films, but it's a very important film, one that set the template for how Marvel superhero movies would look for the next 20 years. (20th anniversary)"),
+        (5, "The visuals even out the problems with characters and storyline concepts for a first-class comic book adaptation that impresses more often than not."),
+        (6, "Venom could have brought a certain amount of sitcom silliness to a grimmer, grittier part of this world. But in the end, what might have been a bold and bracing new thing winds up being more of the same."),
+        (6, "The movie becomes about Eddie and Venom coming to an arrangement. There is a lot of humour, including a sly riff on the Batman movies from Marvel rival DC Comics."),
+        (7, "It is one of the best movies to ever come out of the Marvel Cinematic Universe, balancing engaging action set pieces and witty dialogue with intelligent character studies and ethical debates.");
